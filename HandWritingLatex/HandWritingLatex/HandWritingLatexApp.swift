a8c067e156b57836fedfb09d1cbe5c17866c0188
Pay4att <1718014302@qq.com>
2025-12-20 00:38:04 +0800

    feat: 添加项目的基本结构和初始代码，包括应用程序主界面和配色方案

---
+++ HandWritingLatex/HandWritingLatex/HandWritingLatexApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandWritingLatexApp.swift
+//  HandWritingLatex
+//
+//  Created by Pay4att on 2025/12/20.
+//
+
+import SwiftUI
+
+@main
+struct HandWritingLatexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
